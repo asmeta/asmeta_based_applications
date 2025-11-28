433c0caa0eea120e04906730c9f062e6ab72d7d1
silviabonfanti <silvia.bonfanti@unibg.it>
2025-11-28 17:49:31 +0100

    models uploaded

---
+++ MRM_CBD/MRMModels/compartment.asm
@@ -0,0 +1,62 @@
+asm compartment
+
+import  StandardLibrary
+
+signature:
+
+enum domain LedLights = {OFF | ON | BLINKING}
+
+enum domain States = {INIT | NORMAL}
+controlled state: States
+	
+//IN from patient
+monitored compartmentOpen:Natural -> Boolean //free monitored variable -> set in the script
+
+//IN from pillbox
+monitored outMess: Natural ->  String
+monitored led: Natural ->  LedLights
+
+//Compartment number
+monitored myID: Natural //free monitored variable -> set in the script
+controlled myID_Cont: Natural
+
+//OUT to patient 
+out ledStatus: Natural -> LedLights
+out displayMessage: Natural -> String
+
+// OUT pillbox
+out openSwitch: Natural -> Boolean
+
+
+definitions:
+
+
+//Check semantic consistency between the status of redLed and the given the out message
+invariant inv_A_compOutMess1 over outMess: contains(outMess(myID_Cont), "Take")  implies (led(myID_Cont) = ON or led(myID_Cont) = BLINKING)
+invariant inv_A_compOutMess2 over outMess: contains(outMess(myID_Cont), "Close")  implies led(myID_Cont) = BLINKING 
+invariant inv_A_compOutMess3 over outMess: (outMess(myID_Cont)="" or contains(outMess(myID_Cont), "taken") or contains(outMess(myID_Cont), "missed")) implies led(myID_Cont) = OFF 
+
+//Check semantic consistency between the status of redLed and the given out message
+invariant inv_G_compDispMess1 over displayMessage: contains(displayMessage(myID_Cont), "Take")  implies (ledStatus(myID_Cont) = ON or ledStatus(myID_Cont) = BLINKING)
+invariant inv_G_compDispMess2 over displayMessage: contains(displayMessage(myID_Cont), "Close")  implies ledStatus(myID_Cont) = BLINKING 
+invariant inv_G_compDispMess3 over displayMessage: (displayMessage(myID_Cont)="" or contains(displayMessage(myID_Cont), "taken") or contains(displayMessage(myID_Cont), "missed")) implies ledStatus(myID_Cont) = OFF 
+
+
+	main rule r_Main =
+		if state = INIT then
+			par
+				if (myID_Cont=undef) then
+						myID_Cont := myID
+				endif
+				state:=NORMAL
+			endpar
+		else
+					par
+						ledStatus(myID_Cont) := led(myID_Cont)
+						displayMessage(myID_Cont) := outMess(myID_Cont)
+						openSwitch(myID_Cont) := compartmentOpen(myID_Cont)
+					endpar
+		endif
+	
+default init s0:
+	function state = INIT
